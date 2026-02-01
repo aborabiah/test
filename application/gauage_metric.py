68c36b7dd491a1e9ec3a0ef71006ed54aa7d1540
Osama Alrabiah <rsrabiah@gmail.com>
2026-02-01 07:44:21 +0300

    Fix duplicate

--- application/gauage_metric.py
+++ application/gauage_metric.py
@@ -1,10 +1,12 @@
 import http.server
+import time
 from prometheus_client import start_http_server, Gauge
 REQUEST_IN_PROGRESS = Gauge("app_requests_in_progress", "Number of requests currently being processed")
 
 class HandleRequests(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         REQUEST_IN_PROGRESS.inc()
+        time.sleep(5)
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
