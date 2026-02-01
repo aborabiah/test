5eaba2ab9f57eedb5af68c1b3d35cf292317cb35
Osama Alrabiah <rsrabiah@gmail.com>
2026-02-01 08:07:56 +0300

    Fix duplicate

--- application/gauage_metric.py
+++ application/gauage_metric.py
@@ -12,9 +12,8 @@ class HandleRequests(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        REQUEST_LAST_EXECUTION_TIME.set(time.time())
-        # Fixed HTML nesting: </h2> should come before </center>
-        # Encoded the string to bytes directly
+        #REQUEST_LAST_EXECUTION_TIME.set(time.time())
+        REQUEST_LAST_EXECUTION_TIME.set_to_current_time()
         message = "<html><head><title>First Python Application</title></head><body style='color: #333; margin-top: 30px;'><center><h2>Welcome to our first Python application.</h2></center></body></html>"
         
         self.wfile.write(bytes(message, "utf-8"))
